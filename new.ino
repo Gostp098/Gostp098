62fd1c527d2c8b46b033b3753df989a76f18193e
mariem <mariemhilali17@gmail.com>
2023-12-23 17:51:29 +0100

    void changed

--- new.ino
+++ new.ino
@@ -1,32 +1,21 @@
-// Pin definitions
-const int test = A6;
 
-const int lockdownPin = A3;
-const int startButtonPin = A1;      
-const int stopButtonPin = A0;
+
 const int motorForwardPin = 1;
 const int motorBackwardPin = 2;
 const int waterPumpPin = 3;
 const int waterOutPumpPin = 4;
 const int spinningPin = 5;
-const int DoorContactPin = 6; // Output for door lock operation
+const int DoorContactPin = 6; 
 
-
-// Flag to track whether testing has been completed
 bool testingCompleted = false;
 
 void setup() {
-  pinMode(motorForwardPin, OUTPUT);
+ pinMode(motorForwardPin, OUTPUT);
   pinMode(motorBackwardPin, OUTPUT);
   pinMode(waterPumpPin, OUTPUT);
   pinMode(waterOutPumpPin, OUTPUT);
   pinMode(spinningPin, OUTPUT);
   pinMode(DoorContactPin, OUTPUT);
-
-  pinMode(lockdownPin, INPUT_PULLUP);
-  pinMode(startButtonPin, INPUT_PULLUP);
-  pinMode(stopButtonPin, INPUT_PULLUP);
-
 
 
    digitalWrite(motorForwardPin, 1);
@@ -35,209 +24,134 @@ void setup() {
   digitalWrite(waterOutPumpPin, 1);
   digitalWrite(DoorContactPin, 1);
   digitalWrite(spinningPin, 1);
+}
+
+void loop() {
+ // Check if testing has been completed
+ if (!testingCompleted) {
+
+    // Call each test procedure in sequence
+ 
+      digitalWrite(DoorContactPin, 0);
+    Washing();
+    vidange();
+    rinsing();
+    essorage();
+    // Set the flag to indicate testing is completed
+           delay(60000); // 1min
+
+      digitalWrite(DoorContactPin, 1);
+
+    testingCompleted = true;
+
+ }
+
+ // Optional: Add additional code here if needed
+
+ // Exit the loop and stop the program
+ return;
+}
+
+
+void Washing() {
+   digitalWrite(waterPumpPin, 0); 
+/*
+    
+
+
+ const int cycleCount = 2; // Number of washing cycles
+
+  for (int cycle = 0; cycle < cycleCount; cycle++) {
+    // Forward
+    digitalWrite(motorForwardPin, 0);
+    delay(30000);  // 30s in milliseconds
+    digitalWrite(motorForwardPin, 1);
+    delay(2000);
+
+    // Backward
+    digitalWrite(motorBackwardPin, 0);
+    delay(30000);  // 30s in milliseconds
+    digitalWrite(motorBackwardPin, 1);
+    delay(2000);
+  }
+*/
+    digitalWrite(waterPumpPin, 1); 
+    delay(2000); // A3ML DELAY BIN PRELAV O VIDABGE     
+
+
 
 }
 
-void loop() { 
-  // Check if testing has been completed
-  int lockdownState = digitalRead(lockdownPin);
-  int startButtonState = digitalRead(startButtonPin);
-  int stopButtonState = digitalRead(stopButtonPin);  // Add this line
 
 
+void vidange() {
+ // Assuming a pin has been connected to a relay
+digitalWrite(waterOutPumpPin, 0);
 
-
-//if (lockdownState == 1 && startButtonState == 1 && !testingCompleted ) {
-if (!testingCompleted ) {
-
-digitalWrite(DoorContactPin, 0);
-   
-   
-
-  
-   ///////////// Washing ////////////////
-
-  digitalWrite(waterPumpPin, 0); 
-      digitalWrite(motorForwardPin, 0); 
-        delay(30000);  // 4 minutes in milliseconds
-   digitalWrite(motorForwardPin, 1);
-   delay(5000);  // DELAY bin lymin o lisar
-
-    
-      // Backward motion (adjust the duration as needed)
-      digitalWrite(motorBackwardPin, 0); // Start motor backward
-           delay(30000);    // 4 minutes in milliseconds
-      digitalWrite(motorBackwardPin, 1); // Stop motor backward
-     delay(2000);  // DELAY bin lymin o lisar
-
-      
-      // Forward motion (adjust the duration as needed)
-      digitalWrite(motorForwardPin, 0); // Start motor forward
-          delay(30000);   // 4 minutes in milliseconds
-      digitalWrite(motorForwardPin, 1); // Stop motor forward
-      delay(2000); // DELAY bin lymin o lisar
-
-
-      // Backward motion (adjust the duration as needed)
-      digitalWrite(motorBackwardPin, 0); // Start motor backward
-            delay(30000);    // 4 minutes in milliseconds
-      digitalWrite(motorBackwardPin, 1); // Stop motor backward
-
-
-        digitalWrite(waterPumpPin, 1); 
-
-     delay(2000); // A3ML DELAY BIN PRELAV O VIDABGE     
-
-
-
-//:::::::::::: vidange ////////////////////////
- digitalWrite(waterOutPumpPin, 0);
-
-    digitalWrite(motorForwardPin, 0); // Start motor forward
+ /*   digitalWrite(motorForwardPin, 0); // Start motor forward
                 delay(90000); //1,5 min
     digitalWrite(motorForwardPin, 1); // Stop motor forward
-
- delay(5000);
-
-
+   delay(5000);
       digitalWrite(motorBackwardPin, 0); // Start motor backward
                 delay(90000); //1,5 min
       digitalWrite(motorBackwardPin, 1); // Stop motor backward
+
+*/
+
+ const int cycleCount = 3; // Number of washing cycles
+
+  for (int cycle = 0; cycle < cycleCount; cycle++) {
+    // Forward
+    digitalWrite(motorForwardPin, 0);
+    delay(30000);  // 30s in milliseconds
+    digitalWrite(motorForwardPin, 1);
+    delay(2000);
+
+    // Backward
+    digitalWrite(motorBackwardPin, 0);
+    delay(30000);  // 30s in milliseconds
+    digitalWrite(motorBackwardPin, 1);
+    delay(2000);
+  }
+
 
 
       digitalWrite(waterOutPumpPin, 1);
 
 
 
-  delay(5000);
+  delay(2000);
 
 
+}
 
-  
-
-
-
-   ///////////// Washing ////////////////
-
+void rinsing() {
   digitalWrite(waterPumpPin, 0); 
-      digitalWrite(motorForwardPin, 0); 
-        delay(240000);  // 4 minutes in milliseconds
-   digitalWrite(motorForwardPin, 1);
-   
-   delay(5000);  // DELAY bin lymin o lisar
-
-    
-      // Backward motion (adjust the duration as needed)
-      digitalWrite(motorBackwardPin, 0); // Start motor backward
-        delay(240000);  // 4 minutes in milliseconds
-      digitalWrite(motorBackwardPin, 1); // Stop motor backward
-     delay(2000);  // DELAY bin lymin o lisar
-
-      
-      // Forward motion (adjust the duration as needed)
-      digitalWrite(motorForwardPin, 0); // Start motor forward
-        delay(240000);  // 4 minutes in milliseconds
-      digitalWrite(motorForwardPin, 1); // Stop motor forward
-      delay(2000); // DELAY bin lymin o lisar
 
 
-      // Backward motion (adjust the duration as needed)
-      digitalWrite(motorBackwardPin, 0); // Start motor backward
-          delay(240000);  // 4 minutes in milliseconds
-      digitalWrite(motorBackwardPin, 1); // Stop motor backward
-     delay(2000); // A3ML DELAY BIN PRELAV O VIDABGE     
+  const int cycleCount = 2; // Number of cycles each cycle is 1 min
 
-    
-    
+  for (int cycle = 0; cycle < cycleCount; cycle++) {
+    // Forward
+    digitalWrite(motorForwardPin, 0);
+    delay(30000);  // 30s in milliseconds
+    digitalWrite(motorForwardPin, 1);
+    delay(2000);
 
-  
-
-//:::::::::::: vidange ////////////////////////
- digitalWrite(waterOutPumpPin, 0);
-
-    digitalWrite(motorForwardPin, 0); // Start motor forward
-                delay(90000); //1,5 min
-    digitalWrite(motorForwardPin, 1); // Stop motor forward
-
- delay(5000);
-
-
-      digitalWrite(motorBackwardPin, 0); // Start motor backward
-                delay(90000); //1,5 min
-      digitalWrite(motorBackwardPin, 1); // Stop motor backward
-
-
-      digitalWrite(waterOutPumpPin, 1);
-
-
-
-  delay(5000);
-
-
-
+    // Backward
+    digitalWrite(motorBackwardPin, 0);
+    delay(30000);  // 30s in milliseconds
+    digitalWrite(motorBackwardPin, 1);
+    delay(2000);
+  }
 
      
-/////////// RINSING      ///////
-
- digitalWrite(waterPumpPin, 0); 
-      digitalWrite(motorForwardPin, 0); 
-        delay(120000);  // 2 minutes in milliseconds
-   digitalWrite(motorForwardPin, 1);
-  digitalWrite(waterPumpPin, 1); 
-   delay(5000);  // DELAY bin lymin o lisar
-
-    
-      // Backward motion (adjust the duration as needed)
-      digitalWrite(motorBackwardPin, 0); // Start motor backward
-        delay(120000);  // 2 minutes in milliseconds
-      digitalWrite(motorBackwardPin, 1); // Stop motor backward
-     delay(2000);  // DELAY bin lymin o lisar
-
-
-
-//:::::::::::: vidange ////////////////////////
- digitalWrite(waterOutPumpPin, 0);
-
-    digitalWrite(motorForwardPin, 0); // Start motor forward
-                delay(90000); //1,5 min
-    digitalWrite(motorForwardPin, 1); // Stop motor forward
-
- delay(5000);
-
-
-      digitalWrite(motorBackwardPin, 0); // Start motor backward
-                delay(90000); //1,5 min
-      digitalWrite(motorBackwardPin, 1); // Stop motor backward
-
-
-      digitalWrite(waterOutPumpPin, 1);
-
-
-
-  delay(5000);
-
-
-
-
      
-/////////// RINSING      ///////
+      digitalWrite(waterPumpPin, 1); 
+delay(2000);
+}
 
- digitalWrite(waterPumpPin, 0); 
-      digitalWrite(motorForwardPin, 0); 
-        delay(120000);  // 2 minutes in milliseconds
-   digitalWrite(motorForwardPin, 1);
-  digitalWrite(waterPumpPin, 1); 
-   delay(5000);  // DELAY bin lymin o lisar
-
-    
-      // Backward motion (adjust the duration as needed)
-      digitalWrite(motorBackwardPin, 0); // Start motor backward
-        delay(120000);  // 2 minutes in milliseconds
-      digitalWrite(motorBackwardPin, 1); // Stop motor backward
-     delay(2000);  // DELAY bin lymin o lisar
-
-
-////////// essorage - spinning
+void essorage() {
 
      digitalWrite(waterOutPumpPin, 0);
       digitalWrite(spinningPin, 0);
@@ -247,27 +161,7 @@ digitalWrite(DoorContactPin, 0);
       digitalWrite(waterOutPumpPin, 1);
       digitalWrite(spinningPin, 1);
 
-      //FIN
+   /*   //FIN
        delay(60000); // 1min
-       digitalWrite(DoorContactPin, 1);
-
-testingCompleted = true;
-
-   
-     }
-  /*   
-      if (stopButtonState == 1) {
-  // Code to stop everything
-  digitalWrite(motorForwardPin, 1);
-  digitalWrite(motorBackwardPin, 1);
-  digitalWrite(waterPumpPin, 1);
-  digitalWrite(waterOutPumpPin, 1);
-  digitalWrite(spinningPin, 1);
-         delay(60000); // 1min 
-  digitalWrite(DoorContactPin, 1);
-      }
-   */
-  
- 
-  return;
+       digitalWrite(DoorContactPin, 1);*/
 }
